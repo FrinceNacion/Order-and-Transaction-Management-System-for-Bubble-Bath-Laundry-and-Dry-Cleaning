83bfb47e6f4f936d981b982bebbf11686d7db4f4
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-04-27 16:19:25 +0800

    Update: replace button with div for due date toggle in DueDatePicker component

--- src/components/DueDatePicker.jsx
+++ src/components/DueDatePicker.jsx
@@ -1,7 +1,6 @@
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/style.css";
 import { useState } from "react";
-import { is } from "react-day-picker/locale";
 
 function DueDatePicker() {
     const [selected, setSelected] = useState();
@@ -11,10 +10,10 @@ function DueDatePicker() {
         <div>
             <div className="d-flex flex-column">
                 <label htmlFor="dueDateBtn" className="form-label text-black">Due Date</label>
-                <button id="dueDateBtn" onClick={() => setVisible((prev) => !prev)} className="btn btn-secondary d-flex flex-row gap-2">
+                <div id="dueDateBtn" onClick={() => setVisible((prev) => !prev)} className="btn btn-secondary d-flex flex-row gap-2">
                     <i className="bi bi-calendar"></i>
                     <p className="m-0">{selected ? `Selected: ${selected.toLocaleDateString()}` : "Select"}</p>
-                </button>
+                </div>
             </div>
             <DayPicker
                 className={!isVisible ? 'd-none' : ''}
