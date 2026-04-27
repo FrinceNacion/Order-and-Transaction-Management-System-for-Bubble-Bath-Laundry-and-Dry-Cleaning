49294119e992cb842397510451ca7f75e869b7d5
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-04-27 14:06:31 +0800

    Update: enhance DueDatePicker component with visibility toggle and improved layout

--- src/components/DueDatePicker.jsx
+++ src/components/DueDatePicker.jsx
@@ -1,20 +1,33 @@
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/style.css";
+import { useState } from "react";
+import { is } from "react-day-picker/locale";
 
 function DueDatePicker() {
-  const [selected, setSelected] = useState<Date>(null);
+    const [selected, setSelected] = useState();
+    const [isVisible, setVisible] = useState(false);
 
-  return (
-    <DayPicker
-      mode="single"
-      selected={selected}
-      onSelect={setSelected}
-      disabled={{ before: new Date() }}
-      footer={
-        selected ? `Selected: ${selected.toLocaleDateString()}` : "Select the due date."
-      }
-    />
-  );
+    return (
+        <div>
+            <div className="d-flex flex-column">
+                <label htmlFor="dueDateBtn" className="form-label text-black">Due Date</label>
+                <button id="dueDateBtn" onClick={() => setVisible((prev) => !prev)} className="btn btn-secondary d-flex flex-row gap-2">
+                    <i className="bi bi-calendar"></i>
+                    <p className="m-0">{selected ? `Selected: ${selected.toLocaleDateString()}` : "Select"}</p>
+                </button>
+            </div>
+            <DayPicker
+                className={!isVisible ? 'd-none' : ''}
+                mode="single"
+                selected={selected}
+                onSelect={setSelected}
+                disabled={{ before: new Date() }}
+                footer={
+                    selected ? `Selected: ${selected.toLocaleDateString()}` : "Select"
+                }
+            />
+        </div>
+    );
 }
 
 export default DueDatePicker;
